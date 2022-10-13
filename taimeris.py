edf3952f411f83933e522251a32e03f6b239e13f
Raivis <71266644+raivis-rasnacs@users.noreply.github.com>
2022-10-13 18:39:21 +0300

    Update taimeris.py

--- taimeris.py
+++ taimeris.py
@@ -4,8 +4,7 @@ import sleep from time
 
 # Funkcija veic laika atskaiti
 def atskaite(h, m, s):
-  pass 
-  # TODO: Sastādi taimera algoritmu!
+  pass # TODO: Sastādi taimera algoritmu!
 
 # Ievada stundas, minūtes un sekundes
 h = int(input("Stundas: "))
