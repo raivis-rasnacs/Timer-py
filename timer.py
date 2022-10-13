ef0796fb970ce40c1fcb7d872081734c039b5661
Raivis <71266644+raivis-rasnacs@users.noreply.github.com>
2022-10-13 17:49:16 +0300

    Create timer.py

---
+++ timer.py
@@ -0,0 +1,15 @@
+# !!! Uzvārds, klase !!!
+
+import sleep from time
+
+# Funkcija veic laika atskaiti
+def taimeris(h, m, s):
+  pass 
+  # TODO: Sastādi taimera algoritmu!
+
+# Ievada stundas, minūtes un sekundes
+h = int(input("Stundas: "))
+m = int(input("Minūtes: "))
+s = int(input("Sekundes: "))
+
+taimeris(h, m, s)
