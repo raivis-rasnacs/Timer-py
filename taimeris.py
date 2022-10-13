adf4c3e64ef474c8c538a808dc67f589b2e74a40
Raivis <71266644+raivis-rasnacs@users.noreply.github.com>
2022-10-13 18:48:31 +0300

    Update taimeris.py

--- taimeris.py
+++ taimeris.py
@@ -1,14 +1,27 @@
-# !!! Uzvārds, klase !!!
+# --- uzvārds, klase ---
 
 import sleep from time
 
-# Funkcija veic laika atskaiti
+# Laika atskaite
 def atskaite(h, m, s):
   pass # TODO: Sastādi taimera algoritmu!
 
-# Ievada stundas, minūtes un sekundes
-h = int(input("Stundas: "))
-m = int(input("Minūtes: "))
-s = int(input("Sekundes: "))
+def ievads():
+  # Ievada stundas, minūtes un sekundes
+  h = int(input("Stundas: "))
+  m = int(input("Minūtes: "))
+  s = int(input("Sekundes: "))
+  
+  if dati_korekti(h, m, s) == True:
+    atskaite(h, m, s)
+  else:
+    print("Kļūda!")
+  
+def dati_korekti(stundas, minutes, sekundes):
+  
+  # TODO: Sastādi datu validācijas algoritmu! 
+  
+  return # Atgriež True, ja dati korekti, citādi False
 
-atskaite(h, m, s)
+
+ievads()
